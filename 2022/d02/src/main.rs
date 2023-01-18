fd7af748264d0b2a1417726246cd142338cfc3eb
Victor Guichard <guichardvictor@gmail.com>
2023-01-18 23:11:45 +0100

    feat: add day 2 part 2 solution

--- 2022/d02/src/main.rs
+++ 2022/d02/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn part_1() {
     let input = include_bytes!("../input");
     let score = input.split(|b|*b == b'\n')
         .map(|line| ((line[0] - b'A') as i16, (line[2] - b'X') as i16,))
@@ -6,4 +6,19 @@ fn main() {
         .sum::<i16>();
 
     println!("{}", score)
+}
+
+fn part_2() {
+    let input = include_bytes!("../input");
+    let score = input.split(|b|*b == b'\n')
+        .map(|line| ((line[0] - b'A') as i16, (line[2] - b'X') as i16,))
+        .map(|(opponent, mine)| 3 * mine + 1 + (2 + mine + opponent).rem_euclid(3))
+        .sum::<i16>();
+
+    println!("{}", score)
+}
+
+fn main() {
+    part_1();
+    part_2();
 }
