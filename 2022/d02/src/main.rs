6a1cae61e86b6622d4867e0507bf300b1ebd00d3
Victor Guichard <guichardvictor@gmail.com>
2023-01-18 23:02:50 +0100

    feat: add day 2 solution
    commit d469b1a939a0df46fa8066fb3428ea2b92f42dea
    Author: Victor Guichard <guichardvictor@gmail.com>
    Date:   Wed Jan 18 23:01:47 2023 +0100
    
        feat: add day 2 solution

---
+++ 2022/d02/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_bytes!("../input");
+    let score = input.split(|b|*b == b'\n')
+        .map(|line| ((line[0] - b'A') as i16, (line[2] - b'X') as i16,))
+        .map(|(opponent, mine)| 1 + mine + 3 * (1 + mine - opponent).rem_euclid(3))
+        .sum::<i16>();
+
+    println!("{}", score)
+}
